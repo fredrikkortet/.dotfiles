09f3735c5357222eaf02a10b4ce00d562a92b8cf
fredrik kortetjärvi <49959669+fredrikkortet@users.noreply.github.com>
2021-02-17 20:37:15 +0100

    fixed

--- config/awesome/autostart.sh
+++ config/awesome/autostart.sh
@@ -9,11 +9,9 @@ function run {
 #run "xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal"
 #run "xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
 run "nm-applet"
-run "xfce4-power-manager"
 run "blueberry-tray"
-run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+run "/usr/bin/lxpolkit"
 run "numlockx on"
 run "volumeicon"
-run "nitrogen --restore"
 run "signal-desktop"
-
+feh --rendomize --bg-fill /home/tipparn/.wallpaper
