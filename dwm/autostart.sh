c2f52ec04ae9e51bcbe3380eadc8399f8d3994fa
fredrik kortetjärvi <49959669+fredrikkortet@users.noreply.github.com>
2021-03-21 15:38:27 +0100

    added duns

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -10,14 +10,14 @@ function run {
 #run "xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
 #nm-applet &
 #run "blueberry-tray"
-/usr/bin/lxpolkit &
+feh --randomize --bg-fill /home/tipparn/.wallpaper &
+$HOME/.dwm/dwmblocks/dwmblocks &
 numlockx on
 #volumeicon
-signal-desktop &
-feh --randomize --bg-fill /home/tipparn/.wallpaper &
 xset r rate 300 50 &
-$HOME/.dwm/dwmblocks/dwmblocks &
 setxkbmap -option ctrl:nocaps &
 xmodmap -e "keycode 66 = Escape" &
 picom --experimental-backends --xrender-sync-fence &
+/usr/bin/lxpolkit &
+signal-desktop &
 #run "sudo $HOME/.config/awesome/scripts/init_blackwidow.py"
