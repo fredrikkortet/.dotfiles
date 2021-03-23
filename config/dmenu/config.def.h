f44c6fa701ac06fd78e4c2b4b839afb3fda9509d
Fredrik Kortetjärvi <49959669+fredrikkortet@users.noreply.github.com>
2021-03-23 19:28:46 +0100

    git again

--- config/dmenu/config.def.h
+++ config/dmenu/config.def.h
@@ -12,8 +12,8 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeSelHighlight] = { "#ebcb8b", "#81a1c1" },
+	[SchemeNormHighlight] = { "#d08770", "#2e3440" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
--- dwm/dwm-6.2/config.def.h
+++ dwm/dwm-6.2/config.def.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
-#define TERMINAL "alacritty"
+#define TERMINAL "urxvt"
+#define BROWSER  "librewolf"
+#define EDITOR   "nvim"
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -10,19 +12,19 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char col1[]            = "#ffffff";
-static const char col2[]            = "#ffffff";
-static const char col3[]            = "#ffffff";
-static const char col4[]            = "#ffffff";
-static const char col5[]            = "#ffffff";
-static const char col6[]            = "#ffffff";
+static const char *fonts[]          = { "dejavu sans:size=12" };
+static const char dmenufont[]       = "dejavu sans:size=12";
+static const char col_gray1[]       = "#2e3440";//nord0 dark gray 
+static const char col_gray2[]       = "#3b4252";//nord1 lighter gray
+static const char col_gray3[]       = "#eceff4";//nord6 almost white
+static const char col_gray4[]       = "#eceff4";//nord6 almost white
+static const char col_cyan[]        = "#5e81ac";//nord10 dark blue
+static const char col1[]            = "#88c0d0";//nord8 light light blue
+static const char col2[]            = "#81a1c1";//light blue
+static const char col3[]            = "#b48ead";//purple
+static const char col4[]            = "#a3be8c";//green
+static const char col5[]            = "#bf616a";//red
+static const char col6[]            = "#ebcb8b";//yellow
 
 enum { SchemeNorm, SchemeCol1, SchemeCol2, SchemeCol3, SchemeCol4,
        SchemeCol5, SchemeCol6, SchemeSel }; /* color schemes */
@@ -30,12 +32,12 @@ enum { SchemeNorm, SchemeCol1, SchemeCol2, SchemeCol3, SchemeCol4,
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm]  = { col_gray3, col_gray1, col_gray2 },
-	[SchemeCol1]  = { col1,      col_gray1, col_gray2 },
-	[SchemeCol2]  = { col2,      col_gray1, col_gray2 },
-	[SchemeCol3]  = { col3,      col_gray1, col_gray2 },
-	[SchemeCol4]  = { col4,      col_gray1, col_gray2 },
-	[SchemeCol5]  = { col5,      col_gray1, col_gray2 },
-	[SchemeCol6]  = { col6,      col_gray1, col_gray2 },
+	[SchemeCol1]  = { col1,      col_gray1, col_gray2 },// light light blue
+	[SchemeCol2]  = { col2,      col_gray1, col_gray2 },//light blue
+	[SchemeCol3]  = { col3,      col_gray1, col_gray2 },//color purple
+	[SchemeCol4]  = { col4,      col_gray1, col_gray2 },//color green
+	[SchemeCol5]  = { col5,      col_gray1, col_gray2 },//color red
+	[SchemeCol6]  = { col6,      col_gray1, col_gray2 },//color yellow
 	[SchemeSel]   = { col_gray4, col_cyan,  col_cyan  },
 };
 
@@ -55,7 +57,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -77,14 +79,13 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col2, "-sf", col_gray4, NULL };
 
 #include <X11/XF86keysym.h>//add XF86 keys
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          SHCMD(TERMINAL)  },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -94,20 +95,17 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,             XK_f,      fullscreen,     {0} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
+    { MODKEY,                       XK_x,      spawn,          SHCMD("$HOME/.config/rofi/powermenu/powermenu.sh") },
+	//tags
+    TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
@@ -116,9 +114,23 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} },//restart without logout 
-
+	// super+shift
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          SHCMD(TERMINAL)  },
+	{ MODKEY|ShiftMask,             XK_f,      fullscreen,     {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_Escape, quit,           {0} },
+    
+    // application section
+    { MODKEY|ShiftMask,             XK_w,       spawn,          SHCMD(BROWSER) },
+    { MODKEY|ShiftMask,             XK_e,       spawn,          SHCMD(TERMINAL " -e " EDITOR) },
+    { MODKEY|ShiftMask,             XK_d,       spawn,          SHCMD("thunar") },
+    { MODKEY|ShiftMask,             XK_v,       spawn,          SHCMD("pavucontrol") },
+    { MODKEY|ShiftMask,             XK_b,       spawn,          SHCMD("bluetoothdm") },
+    { MODKEY|ShiftMask,             XK_n,       spawn,          SHCMD("networkmanager_dmenu") },
+    
     //special keys
     {0, XF86XK_Calculator,          spawn,  SHCMD(TERMINAL " -e bc") },
     {0, XF86XK_AudioMute,           spawn,  SHCMD("pamixer -t") },
@@ -126,13 +138,12 @@ static Key keys[] = {
     {0, XF86XK_AudioRaiseVolume,    spawn,  SHCMD("pamixer -i 2") },
     {0, XF86XK_AudioMicMute,        spawn,  SHCMD("pactl set-source-mute @DEAFUALT_SOURCE@ toggle") },
     {0, XF86XK_MonBrightnessDown,   spawn,  SHCMD("brightnessctl s 5%-") },
-    {0, XF86XK_MonBrightnessUp,     spawn,  SHCMD("brightnessctl s +10%") },
+    {0, XF86XK_MonBrightnessUp,     spawn,  SHCMD("brightnessctl s +5%") },
     {0, XF86XK_AudioPrev,           spawn,  SHCMD("") },
     {0, XF86XK_AudioPlay,           spawn,  SHCMD("") },
     {0, XF86XK_AudioNext,           spawn,  SHCMD("") },
-
-    {MODKEY,                        XK_x,   spawn,      SHCMD("$HOME/.config/rofi/powermenu/powermenu.sh") },
     
+    { MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} },//restart without logout 
 };
 
 /* button definitions */
